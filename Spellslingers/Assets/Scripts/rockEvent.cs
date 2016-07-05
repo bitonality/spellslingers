82479ead74e853127c7c7066fe3ae6dcd94a52a2
Frankie Salmick <frank@salmick.com>
2016-07-05 13:26:34 -0700

    Started rock

---
+++ Spellslingers/Assets/Scripts/rockEvent.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rockEvent : MonoBehaviour {
+    int framesSinceLastRock = 0;
+	
+	// Update is called once per frame
+	void Update () {
+        framesSinceLastRock += 1;
+	}
+}
