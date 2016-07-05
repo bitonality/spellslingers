1e7538983eb825a1929502a25cbe275697843cf9
George Higashiyama <george@higashiyama.us>
2016-07-05 13:27:57 -0700

    Bouncy wall + explosions

---
+++ Spellslingers/Assets/Scripts/HexCollide.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexCollide : MonoBehaviour
+{
+
+	public GameObject explosion;
+
+ 	void Awake() {
+		if(this.GetComponent<BoxCollider>() != null) 
+			this.GetComponent<BoxCollider>().isTrigger = true;	
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.tag == "Hex")
+		{
+			Instantiate(explosion, transform.position, transform.rotation);
+			Destroy(col.gameObject);
+		
+		}
+	}
+}
