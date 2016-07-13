8d40882c45459ccbc33d984b758298978a79ed05
EspoCar <espomachine@gmail.com>
2016-07-13 12:36:15 -0700

    Made gameobject parameters of all, fixed errors that caused, added two new methods

--- Spellslingers/Assets/aiBase.cs
+++ Spellslingers/Assets/aiBase.cs
@@ -2,8 +2,9 @@
 using System.Collections;
 
 public class aiBase : MonoBehaviour {
-	public Rigidbody rb = GetComponent<Rigidbody>();
-	public void move(Vector3 current,Vector3 goal, double time){
+	public void move(GameObject ai, Vector3 goal, double time){
+		Rigidbody rb = GameObject.GetComponent<Rigidbody>();
+		Vector3 current = ai.transform.position;
 		rb.isKinematic = true;
 		rb.detectCollisions = true;
 		rb.drag = 0;
@@ -11,19 +12,34 @@ public class aiBase : MonoBehaviour {
 		double xDistance = (current.x - goal.x)/time;
 		double yDistance = (current.y - goal.y)/time;
 		double zDistance = (current.z - goal.z)/time;
-		/* Set object's rigid body velocity, wait for variable time, it will move in meters per second, giving you the approximate area the object was intended to be in.
-		 * rb.velocity = new Vector3 (xDistance, yDistance, zDistance);
-		*WaitForSeconds (time);
-		*rb.velocity = new Vector3 (0,0,0);
-*/
+	    rb.velocity = new Vector3 (xDistance, yDistance, zDistance);
+		WaitForSeconds (time);
+		rb.velocity = new Vector3 (0,0,0);
+
 	}
+	public Vector3 getVelocity(GameObject ai){
+		Rigidbody rb = ai.GetComponent<Rigidbody>();
+		return rb.velocity;
+	}
+
+	public bool isMoving(GameObject ai){
+		Rigidbody rb = ai.GetComponent<Rigidbody>();
+		if(rb.velocity.y==0 && rb.velocity.x == 0 && rb.velocity.z == 0){
+			return false;
+			}
+		else{
+			return true;
+			}
+	}
+	public Vector3 getPos(GameObject ai){
+		Rigidbody rb = ai.GetComponent<Rigidbody>();
+	}
+		
 	/*
-	 * public void move(Vector3 position, double time){
 	 * 
 	 * }
 	 * public bool isInDanger()
 	 * public vector3 getPos()
-	 * public vector3 getVolocity()
 	 * public bool isMoving()
 	 * public void shootSpell(spell type, double x, double y, double z, double velocity) //George will do this
  	 */
