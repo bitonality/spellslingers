336d683d00511238fa9474cb405f91c369d5f12e
EspoCar <espomachine@gmail.com>
2016-07-13 12:20:11 -0700

    Fixed Class, added more usability + additional AI support

--- Spellslingers/Assets/aiBase.cs
+++ Spellslingers/Assets/aiBase.cs
@@ -3,18 +3,19 @@ using System.Collections;
 
 public class aiBase : MonoBehaviour {
 	public Rigidbody rb = GetComponent<Rigidbody>();
-	public void move(Vector3 goal, double time){
+	public void move(Vector3 current,Vector3 goal, double time){
 		rb.isKinematic = true;
 		rb.detectCollisions = true;
 		rb.drag = 0;
 		rb.angularDrag = 0;
-		double xDistance = (transform.position.x - goal.x)/time;
-		double yDistance = (transform.position.y - goal.y)/time;
-		double zDistance = (transform.position.z - goal.z)/time;
-		rb.velocity = new Vector3 (xDistance, yDistance, zDistance);
-		WaitForSeconds (10);
-		rb.velocity = new Vector3 (0,0,0);
-
+		double xDistance = (current.x - goal.x)/time;
+		double yDistance = (current.y - goal.y)/time;
+		double zDistance = (current.z - goal.z)/time;
+		/* Set object's rigid body velocity, wait for variable time, it will move in meters per second, giving you the approximate area the object was intended to be in.
+		 * rb.velocity = new Vector3 (xDistance, yDistance, zDistance);
+		*WaitForSeconds (time);
+		*rb.velocity = new Vector3 (0,0,0);
+*/
 	}
 	/*
 	 * public void move(Vector3 position, double time){
