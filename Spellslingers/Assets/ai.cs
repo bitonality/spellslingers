9b8280f61dadd3ef9cd9296aa6ebefea6630482c
Frankie Salmick <frank@salmick.com>
2016-07-14 11:35:24 -0700

    More AI stuff

--- Spellslingers/Assets/ai.cs
+++ Spellslingers/Assets/ai.cs
@@ -2,8 +2,10 @@
 using System.Collections;
 
 public class ai : MonoBehaviour {
-	int level = 1; //1-3, set somewhere else but set here for now.
-	float speed;
+	//Level is 1/2/3, set somewhere else but set here for now.
+	int level = 1; 
+	//Speed is 5/5.5/6 m/s
+	float speed = 4.5 + (0.5 * level);
 
 	void Start () {
 		//If level 3 (hardest), immediately shoot a dizzy spell
@@ -14,11 +16,14 @@ public class ai : MonoBehaviour {
 		//Once every 2/1/.66 seconds, shoot a damage spell
 		shootSpellRepeat(2/level, damage);
 
+		//Once every 3/2/1 shoot a dizzy spell
+		shootSpellRepeat((4 + (-1 * level)), dizzy);
+
+		//Once every 5/4/3 seconds, shoot a disarm spell
+		shootSpellRepeat((6 + (-1 * level)), disarm);
+
 		//Once every 0.33 seconds, check if the AI is in danger
 		checkSafety(0.33);
-
-		//Determine what speed should be. Speed is 5/5.5/6 m/s
-		speed = 4.5 + (0.5 * level);
 	}
 
 	IEnumerator shootSpellRepeat(float secondsBetween, spell spellType)
@@ -43,7 +48,8 @@ public class ai : MonoBehaviour {
 				//Last arg was calculated with equation time = distance/velocity (speed)
 				aiBase.move(this.gameObject, newPosition, (Vector3.Distance(position, newPosition) / speed));
 			} else {
-				Debug.Log ("Not in danger");
+				Debug.Log ("No longer in danger");
+				aiBase.cancelMove ();
 			}
 		}
 	}
