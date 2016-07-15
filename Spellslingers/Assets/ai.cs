49ce5fffe6ec31a481f331b22ed50d13acccc0df
Frank Salmick <frank@salmick.com>
2016-07-15 12:05:28 -0700

    AI testing - large AI base changes

--- Spellslingers/Assets/ai.cs
+++ Spellslingers/Assets/ai.cs
@@ -2,67 +2,69 @@
 using System.Collections;
 
 
+
 public class ai : MonoBehaviour {
 	//Only until we get the spells setup, so we don't get errors
-	spell dizzy = new spell();
-	spell damage = new spell();
-	spell disarm = new spell();
-
-	//Starting here we get actual keep-this code
-	//Include the framework
-	aiBase framework = new aiBase();
+	spell dizzy;
+	spell damage;
+	spell disarm;
+	private System.Random rnd = new System.Random ();
+	//-1 for left, +1 for right, 0 for default
+	private int movementDirection = 0;
 
 	//Level is 1/2/3, set somewhere else but set here for now.
 	static int level = 1; 
 	//Speed is 5/5.5/6 m/s
 	float speed = (4.5F + (0.5F * level));
 
+	public Hex hex1;
+	public float time1;
+	public Hex hex2;
+	public float time2;
+
+	//load hexes into the hashtable on load
+	private Hashtable spells = new Hashtable();
+
 	void Start () {
-		//If level 3 (hardest), immediately shoot a dizzy spell
-		if (level == 3) {
-			//TODO: Make the spell aim at the player
-			framework.shootSpell (dizzy, new Vector3(0, 0, 0));
+		spells.Add (hex1, time1);
+		spells.Add (hex2, time2);
+		Debug.Log ("Started");
+
+		foreach (DictionaryEntry de in spells) {
+			StartCoroutine (ShootSpell ((Hex) de.Key, (float) de.Value));
 		}
-
-		//Once every 2/1/.66 seconds, shoot a damage spell
-		shootSpellRepeat(2/level, damage);
-
-		//Once every 3/2/1 shoot a dizzy spell
-		shootSpellRepeat((4 + (-1 * level)), dizzy);
-
-		//Once every 5/4/3 seconds, shoot a disarm spell
-		shootSpellRepeat((6 + (-1 * level)), disarm);
-
+	
 		//Once every 0.33 seconds, check if the AI is in danger
-		checkSafety(0.33F);
+		InvokeRepeating("checkSafety", 0F, 0.33F);
 	}
 
-	IEnumerator shootSpellRepeat(float secondsBetween, spell spellType)
-	{
-		while(true)
-		{
-			yield return new WaitForSeconds(secondsBetween);
-			//TODO: Have georgie set this up. Also, make it aim at the player
-			framework.shootSpell (spellType, new Vector3(0, 0, 0)); //Again, georgie does this
-		}
+	IEnumerator ShootSpell(Hex h, float delaytime) {
+		yield return new WaitForSeconds (delaytime);
+		shootSpell (h);
 	}
 
-	IEnumerator checkSafety(float secondsBetween)
+
+	//TODO: Bad assumption kys
+	void OnTriggerExit(Collider col) {
+		gameObject.GetComponent<Rigidbody> ().velocity *= -1;
+	}
+
+	//TODO: Make georgie do this
+	public  void shootSpell(Hex hex)
 	{
-		while(true)
-		{
-			yield return new WaitForSeconds(secondsBetween);
-			if (framework.isInDanger ()) {
-				Debug.Log ("In danger, moving");
-				//Move 
-				Vector3 position = this.gameObject.transform.position;
-				Vector3 newPosition = new Vector3 (position.x + 5, position.y, position.z);
-				//Last arg was calculated with equation time = distance/velocity (speed)
-				framework.move(this.gameObject, newPosition, (Vector3.Distance(position, newPosition) / speed));
-			} else {
-				Debug.Log ("No longer in danger");
-				framework.cancelMove (this.gameObject);
-			}
+		Hex proj = Instantiate (hex, gameObject.transform.position + new Vector3(0, 1F, 0), gameObject.transform.rotation) as Hex;
+		proj.GetComponent<Rigidbody>().velocity = (GameObject.FindGameObjectWithTag("MainCamera").transform.position - gameObject.transform.position- new Vector3(0,1F, 0)).normalized * 10;
+	}
+
+	void checkSafety()
+	{
+		if (aiBase.isInDanger ()) {
+			//Move 
+			Vector3 position = this.gameObject.transform.position;
+			Vector3 direction = new Vector3 (0, 0, (float) 200 * this.rnd.Next (-2, 0) * 2 + 3);
+			aiBase.move(this.gameObject, direction);
+		} else {
+			aiBase.cancelMove (this.gameObject);
 		}
 	}
 }
