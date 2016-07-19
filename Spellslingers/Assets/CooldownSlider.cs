5e1091beafc8a89eba4c54ae44d9b8ad4a0fb38d
George Higashiyama <george@higashiyama.us>
2016-07-19 10:51:48 -0700

    UI Tests for sliders

---
+++ Spellslingers/Assets/CooldownSlider.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CooldownSlider : MonoBehaviour {
+
+	GameObject canvas;
+	private float updateTick = 0.1F;
+
+	// Use this for initialization
+	void Start () {
+		canvas = GameObject.FindGameObjectWithTag ("Canvas");
+	}
+
+	public void cooldown(Hex hex, float future) {
+		Slider s = canvas.GetComponent<Slider> ();
+		s.value = 0;
+		float incrementValue = hex.cooldown / updateTick;
+		StartCoroutine ();
+	}
+
+	IEnumerator Increment(float increment, Slider s, float future) {
+		while (future >= Time.time) {
+			yield return new WaitForSeconds (updateTick);
+			s.value += increment;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
