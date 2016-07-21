d8fb200c73df30bccac0610e42d5340067508871
EspoCar <espomachine@gmail.com>
2016-07-21 14:01:21 -0700

    Stun spell reworked,  needs testing, along with damage.

---
+++ Spellslingers/Assets/ForceoffParticles.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForceoffParticles : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.gameObject.GetComponent<ParticleSystem> ().enableEmission = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
