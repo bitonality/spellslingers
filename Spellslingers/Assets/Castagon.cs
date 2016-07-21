d3d7b03d34757bae3c557b468507abd0891d973c
George Higashiyama <george@higashiyama.us>
2016-07-21 14:12:56 -0700

    Castagon queue initial tests

--- Spellslingers/Assets/Castagon.cs
+++ Spellslingers/Assets/Castagon.cs
@@ -1,9 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Castagon : MonoBehaviour {
 
+	public List<CastagonInsepctorEntry> InspectorSpells;
 
+
+	private Queue<CastagonPoint> ActivatedPoints {
+		get;
+		set;
+	}
+
+
+	public void AddPoint(CastagonPoint cp) {
+		this.ActivatedPoints.Enqueue (cp);
+		CheckSpell ();
+	}
+
+	public void CheckSpell() {
+		CastagonPoint cp = ActivatedPoints.Dequeue();
+		for (int i = (InspectorSpells.Count - 1); i >= 0; i--) {
+			CastagonInsepctorEntry potential = InspectorSpells [i];
+			if (cp.CastagonPointID !=  potential.order [potential.order.Count - 1]) {
+				InspectorSpells.RemoveAt (i);
+			} else {
+				if (potential.order.Count == 1) {
+					Debug.Log (potential.name);
+				}
+				potential.order.RemoveAt (potential.order.Count - 1);
+			}
+		}
+	}
 
 	void OnTriggerExit(Collider col) {
 		this.destroy ();
@@ -11,15 +39,23 @@ public class Castagon : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		this.ActivatedPoints = new Queue<CastagonPoint> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 
 	public void destroy() {
 		Destroy (this.gameObject);
 	}
+
+
+	[System.Serializable]
+	public class CastagonInsepctorEntry {
+		public string name;
+		public List<int> order;
+	}
+
 }
---
+++ Spellslingers/Assets/CastagonPoint.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CastagonPoint : MonoBehaviour {
+	public int CastagonPointID;
+
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.tag == "Wand") {
+			Castagon c = gameObject.GetComponentInParent<Castagon> ();
+			c.AddPoint (this);
+		}
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
