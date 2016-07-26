fc1aaf74227fe56aeeaf329a22ed1f64b6400166
EspoCar <espomachine@gmail.com>
2016-07-26 13:16:50 -0700

    AI rotation script + math. Implement with Frankie Later.

---
+++ Spellslingers/Assets/Necromancer/Scripts/DodgeAnimation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DodgeAnimation : MonoBehaviour {
+   public float rotationspeed = 30.0f;
+    public void  Dodge(Vector3 Destination)
+    {
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(Destination, transform.position), rotationspeed);
+        gameObject.GetComponent<Animation>().CrossFade("Run");
+        // Ask Frankie how AI stops moving and work that into the class
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(GameObject.Find("[CameraRig]").transform.position, transform.position), rotationspeed);
+        gameObject.GetComponent<Animation>().CrossFade("Idle1");
+    }
+}
---
+++ Spellslingers/Assets/Necromancer/Scripts/TestAnimations.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestAnimations : MonoBehaviour {
+    public Animation anim;
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animation>();
+        anim.CrossFade("Death1");
+        anim.CrossFade("Walk");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
