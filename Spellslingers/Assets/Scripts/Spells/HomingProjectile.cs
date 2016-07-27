2aa3a6dc61f5fc21af045a493163bb2366152b36
George Higashiyama <george@higashiyama.us>
2016-07-27 11:31:27 -0700

    Homing missile tracking initial test

--- Spellslingers/Assets/Scripts/Spells/HomingProjectile.cs
+++ Spellslingers/Assets/Scripts/Spells/HomingProjectile.cs
@@ -7,33 +7,34 @@ public class HomingProjectile : MonoBehaviour {
     private Transform target;
 	private float sensitivity;
 	private float ConstantMagnitude;
+	private Quaternion LastRotation;
 	private bool update = false;
 
 	public void LaunchProjectile(Transform target, Hex h, float sensitivity, Vector3 controller, GameObject c)
     {
+		h.gameObject.transform.rotation = c.transform.rotation;
         this.target = target;
-		this.sensitivity = sensitivity;
-		h.gameObject.GetComponent<Rigidbody> ().AddForce (controller.magnitude * h.velocity * c.GetComponent<VRTK_InteractGrab>().GetGrabbedObject().transform.FindChild("WandLaunchPoint").transform.forward);
-        Invoke("FlipUpdate", 0.5F);
+		this.sensitivity = 0;
+		gameObject.GetComponent<Rigidbody>().AddForce(c.GetComponent<VRTK_InteractGrab>().GetGrabbedObject().transform.FindChild ("WandLaunchPoint").transform.forward * (float) controller.magnitude * h.velocity);
+		Invoke ("FlipUpdate", 0.1F);
 
     }
 
 	void FlipUpdate() {
 		update = true;
         this.ConstantMagnitude = this.gameObject.GetComponent<Rigidbody>().velocity.magnitude;
+
 	}
+
 
 
     void FixedUpdate ()
 	{
 		if (update) {
-
-            //  var targetRotation = Quaternion.LookRotation(target.position - transform.position);
-             // homingMissile.MoveRotation(Quaternion.RotateTowards(transform.rotation, targetRotation, turn));
 			Quaternion rotation = Quaternion.LookRotation (target.position - transform.position);
-            //transform.rotation = Quaternion.Slerp (transform.rotation, rotation, sensitivity);
-            gameObject.GetComponent<Rigidbody>().MoveRotation(Quaternion.RotateTowards(transform.rotation, rotation, 20));
-
+			Quaternion adjustedRotation = Quaternion.Slerp(transform.rotation, rotation, 1.5F * Time.deltaTime);
+            gameObject.GetComponent<Rigidbody>().MoveRotation(adjustedRotation);
+			gameObject.GetComponent<Rigidbody>().velocity = transform.forward.normalized * this.ConstantMagnitude;
 		}
 	}
 }
