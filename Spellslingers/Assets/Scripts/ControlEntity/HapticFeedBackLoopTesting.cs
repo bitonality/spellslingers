e873332438c1588e8eefd6b78e92bde636868c1e
EspoCar <espomachine@gmail.com>
2016-07-28 10:25:48 -0700

    hapticfeedback Loop testing
    Pushing to access on the Vive.

---
+++ Spellslingers/Assets/Scripts/ControlEntity/HapticFeedBackLoopTesting.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HapticFeedBackLoopTesting : MonoBehaviour
+{
+    SteamVR_Controller.Device device;
+    // Use this for initialization
+    void Start()
+    {
+        startCoroutine(LongVibration(1000, 3000));
+    }
+
+    // Update is called once per frame
+    IEnumerator LongVibration(float length, float strength)
+    {
+        for (float i = 0; i < length; i += Time.deltaTime)
+        {
+            device.TriggerHapticPulse((ushort)Mathf.Lerp(0, 3999, strength));
+            yield return null;
+        }
+    }
+}
