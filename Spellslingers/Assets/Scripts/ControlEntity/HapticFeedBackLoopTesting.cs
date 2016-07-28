4aa52a153e53388678960fd0131441d55d939607
EspoCar <espomachine@gmail.com>
2016-07-28 13:22:07 -0700

    Haptic loop ready for testing, bugfixes.
    Changed how haptic feedback detects device, will change later but this
    is purely for testing purposes.

--- Spellslingers/Assets/Scripts/ControlEntity/HapticFeedBackLoopTesting.cs
+++ Spellslingers/Assets/Scripts/ControlEntity/HapticFeedBackLoopTesting.cs
@@ -3,20 +3,20 @@ using System.Collections;
 
 public class HapticFeedBackLoopTesting : MonoBehaviour
 {
-    /*
+    public SteamVR_Controller.Device device;
     // Initialize here to avoid problems with non-static device methods
     void Start()
     {
-        StartCoroutine(LongVibration(1, 3999));
+        StartCoroutine(TriggerVibration(device, 1, 3999));
     }
 
-	IEnumerator LongVibration(SteamVR_Controller.Device device,	float length, float strength)
+	IEnumerator TriggerVibration(SteamVR_Controller.Device device,	float length, float strength)
     {
         for (float i = 0; i < length; i += Time.deltaTime)
         {
+            Debug.Log("Firing feedback");
             device.TriggerHapticPulse((ushort)Mathf.Lerp(0, 3999, strength));
             yield return null;
         }
     }
-    */
 }
