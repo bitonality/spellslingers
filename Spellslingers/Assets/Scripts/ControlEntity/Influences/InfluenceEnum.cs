6f1461a3cc3c9c0a20e1122058e89a940fd74044
Frank Salmick <frank@salmick.com>
2016-08-03 11:35:40 -0700

    Base for influenceable

---
+++ Spellslingers/Assets/Scripts/ControlEntity/Influences/InfluenceEnum.cs
@@ -0,0 +1,5 @@
+﻿public enum influences
+{
+    DISARM,
+    STUN
+}
---
+++ Spellslingers/Assets/Scripts/ControlEntity/Influences/Influenceable.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface Influenceable {
+
+    object ApplyInfluence(influences inf);
+
+    object RemoveInfluence(influences inf);
+
+    object RemoveInfluenceTimer(influences inf, float time);
+}
