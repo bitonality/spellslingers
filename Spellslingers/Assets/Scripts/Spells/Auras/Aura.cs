74a88ba727cbee323e180ae6559d3d8597c71bbb
George Higashiyama <george@higashiyama.us>
2016-08-04 13:20:35 -0700

    Generlization of particle effects for auras

--- Spellslingers/Assets/Scripts/Spells/Auras/Aura.cs
+++ Spellslingers/Assets/Scripts/Spells/Auras/Aura.cs
@@ -10,8 +10,6 @@ public abstract class Aura : MonoBehaviour {
 
     public GameObject AuraParticleEffect;
 
-    private GameObject InstantiatedParticleEffect;
-
     // Number of seconds for the effect to occur.
     public float Length;
 
@@ -22,10 +20,7 @@ public abstract class Aura : MonoBehaviour {
     }
 
     // How often in seconds to run the aura code in the child class.
-    public virtual float Interval {
-        get;
-        set;
-    }
+    public float Interval;
 
     // Each aura will have a different spawn location, so we need a modifier to alter the spawning location relative to the center of the Target transform
     public Vector3 Position = Vector3.zero;
@@ -37,20 +32,18 @@ public abstract class Aura : MonoBehaviour {
         this.gameObject.transform.position = this.Target.gameObject.transform.position + this.Position;
         this.gameObject.transform.SetParent(this.Target.gameObject.transform);
         if(AuraParticleEffect != null) {
-            Vector3 auraPosition = Target.transform.position;
             if(Target.GetComponent<ControlEntity>().AuraParticleAttachPoint != null) {
-                auraPosition = Target.GetComponent<ControlEntity>().AuraParticleAttachPoint.transform.position;
+                AuraParticleEffect.transform.position = Target.GetComponent<ControlEntity>().AuraParticleAttachPoint.transform.position;
             }
+            AuraParticleEffect.transform.parent = this.Target.transform;
 
-            // Create an explosion at the collision point.
-            this.InstantiatedParticleEffect = (GameObject) Instantiate(AuraParticleEffect, auraPosition, Target.transform.rotation);
         }
     }
     public abstract IEnumerator IntervalAura();
     public virtual void TerminateAura() {
         // Schedule the destruction of the particle system.
-       if(this.InstantiatedParticleEffect != null) {
-            Destroy(this.InstantiatedParticleEffect);
+       if(this.gameObject.GetComponentInChildren<ParticleSystem>() != null) {
+            Destroy(this.gameObject.GetComponentInChildren<ParticleSystem>());
         } 
     }
 
