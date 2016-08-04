6688adbb019ae1b5afcebea876d448c915ebbda8
Frank Salmick <frank@salmick.com>
2016-08-04 14:33:06 -0700

    Improved StateAI state switching

--- Spellslingers/Assets/Scripts/ControlEntity/stateAI.cs
+++ Spellslingers/Assets/Scripts/ControlEntity/stateAI.cs
@@ -4,7 +4,8 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using System;
 
-public class StateAI : ControlEntity {
+public class StateAI : ControlEntity
+{
 
     //time until next state change 
     private float timeUntilChange = 3;
@@ -25,7 +26,8 @@ public class StateAI : ControlEntity {
 
     private Vector3 originalPosition;
 
-    public enum validStates {
+    public enum validStates
+    {
         STARTUP,
         HIT,
         IDLE,
@@ -37,9 +39,11 @@ public class StateAI : ControlEntity {
     }
 
     // Make sure it can't leave the AI boundry.
-    void OnTriggerExit(Collider col) {
+    void OnTriggerExit(Collider col)
+    {
         //Debug.Log (col);
-        if (col.gameObject.tag == "AIBoundry") {
+        if (col.gameObject.tag == "AIBoundry")
+        {
             Debug.Log("Returning to center");
             gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
             gameObject.GetComponent<Rigidbody>().AddForce((originalPosition - gameObject.transform.position) * speed, ForceMode.Impulse);
@@ -49,17 +53,21 @@ public class StateAI : ControlEntity {
     public Queue<validStates> currentAction = new Queue<validStates>();
 
     //Called every 0.02 seconds
-    void FixedUpdate() {
-        if (currentAction.Count <= 0) {
+    void FixedUpdate()
+    {
+        if (currentAction.Count <= 0)
+        {
             //Something went wrong with starting the AI
             Debug.LogWarning("currentAction size is 0, meaning start() was not called before fixedUpdate()");
             return;
         }
-        if (currentAction.Count != 1) {
+        if (currentAction.Count != 1 && timeUntilChange <= Time.time)
+        {
             //Pop leading item off the queue and call justLeft with it
-            justLeft((validStates)currentAction.Dequeue(), getCurrentState());
+            justLeft(currentAction.Dequeue(), getCurrentState());
         }
-        else {
+        else
+        {
             //If there was not a state change, call currentExclusive
             currentExclusive(getCurrentState());
         }
@@ -68,14 +76,12 @@ public class StateAI : ControlEntity {
     }
 
     //Called only when changing state
-    private object justLeft(validStates oldState, validStates newState) {
+    private object justLeft(validStates oldState, validStates newState)
+    {
         //Debug.Log ("Player health: " + this.Enemy.GetComponent<ControlEntity> ().Health);
         //Debug.Log("Changing state from  " + oldState + " to " + newState + " at time " + Time.time);
-        switch (newState) {
-            case validStates.STARTUP:
-                break;
-            case validStates.DANGER:
-                break;
+        switch (newState)
+        {
             case validStates.HIT:
                 currentAction.Enqueue(validStates.DANGER);
                 break;
@@ -85,33 +91,36 @@ public class StateAI : ControlEntity {
                 gameObject.GetComponent<Rigidbody>().AddForce(Destination, ForceMode.Impulse);
                 //Schedule interruptable state change in 3 seconds
                 timeUntilChange = Time.time + 3;
+                currentAction.Enqueue(validStates.PRESHOOT);
                 break;
             case validStates.PRESHOOT:
                 //Stop movement
                 GetComponent<Rigidbody>().velocity = Vector3.zero;
                 //Wait for 1 second
                 timeUntilChange = Time.time + 1;
+                currentAction.Enqueue(validStates.SHOOTING);
                 break;
             case validStates.SHOOTING:
                 //Make sure there is a hex to chose from (otherwise, go back to IDLE stage)
-                if (spellsToShoot.Length != 0) {
+                if (spellsToShoot.Length != 0)
+                {
                     //Pick a hex
                     Hex h = pickHex();
-                    if (CanShoot(h, gameObject)) {
+                    if (CanShoot(h, gameObject))
+                    {
                         //Shoot
-
-
                         CastHex(h, gameObject.transform.GetChild(0).gameObject.transform, this.CurrentTarget().GetComponent<Targetable>().TargetPoint, 2, 5);
-
-                        //Go back to IDLE state
+                        //Go back to POSTSHOOT state
                         currentAction.Enqueue(validStates.POSTSHOOT);
                     }
-                    else {
-                        //Return to PRESHOOT stage
+                    else
+                    {
+                        //Return to PRESHOOT state
                         currentAction.Enqueue(validStates.PRESHOOT);
                     }
                 }
-                else {
+                else
+                {
                     Debug.LogWarning("No spells to choose from, defaulting back to POSTSHOOT");
                     currentAction.Enqueue(validStates.POSTSHOOT);
                 }
@@ -131,10 +140,13 @@ public class StateAI : ControlEntity {
     }
 
     //Called every time, but only if a state change did not occur.
-    private object currentExclusive(validStates state) {
-        if (state == validStates.IDLE) {
+    private object currentExclusive(validStates state)
+    {
+        if (state == validStates.IDLE)
+        {
             //Make sure it isn't in danger
-            if (isInDanger().Count > 0) {
+            if (isInDanger().Count > 0)
+            {
                 currentAction.Enqueue(validStates.DANGER);
             }
         }
@@ -143,17 +155,21 @@ public class StateAI : ControlEntity {
 
     //Pick the best hex for the situation
     //TODO: This
-    private Hex pickHex() {
+    private Hex pickHex()
+    {
         System.Random rnd = new System.Random();
         return spellsToShoot[rnd.Next(0, spellsToShoot.Length)];
     }
 
     //Called regardless if a state change occurred, every time. 
-    private object currentInclusive(validStates state) {
-        if (state == validStates.DANGER) {
+    private object currentInclusive(validStates state)
+    {
+        if (state == validStates.DANGER)
+        {
             //Move out of danger
             ArrayList dangerousSpells = isInDanger();
-            if (dangerousSpells.Count > 0) {
+            if (dangerousSpells.Count > 0)
+            {
                 //Choose the one that is most important
                 //TODO: Sort spells
                 //string[] priorities = new string[] {"Damage", "Disarm", "Stun"};
@@ -161,20 +177,13 @@ public class StateAI : ControlEntity {
                 Vector3 direction = new Vector3(speed * (float)Vector3.Cross(((GameObject)dangerousSpells[0]).transform.position, gameObject.transform.position).normalized.x, UnityEngine.Random.Range(-2, 2), UnityEngine.Random.Range(-2, 2));
                 gameObject.GetComponent<Rigidbody>().AddForce(direction, ForceMode.Impulse);
             }
-            else {
+            else
+            {
                 // Stop movement
                 //gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
                 // Push idle state
                 currentAction.Enqueue(validStates.IDLE);
             }
-        }
-        else if (state == validStates.IDLE && timeUntilChange <= Time.time) {
-            //Change state to preshoot
-            currentAction.Enqueue(validStates.PRESHOOT);
-        }
-        else if (state == validStates.PRESHOOT && timeUntilChange <= Time.time) {
-            //Change to shooting state
-            currentAction.Enqueue(validStates.SHOOTING);
         }
         else if (state == validStates.STARTUP)
         {
@@ -193,12 +202,14 @@ public class StateAI : ControlEntity {
     }
 
     //Returns the object's current state
-    private validStates getCurrentState() {
+    private validStates getCurrentState()
+    {
         return (validStates)currentAction.Peek();
     }
 
     // Use this for initialization
-    public override void Awake() {
+    public override void Awake()
+    {
         base.Awake();
         defaultSpeed = speed;
         originalPosition = gameObject.transform.position;
@@ -206,31 +217,39 @@ public class StateAI : ControlEntity {
         currentAction.Enqueue(validStates.STARTUP);
     }
 
-    public override bool CanShoot(Hex h, GameObject launchPoint) {
+    public override bool CanShoot(Hex h, GameObject launchPoint)
+    {
         //Check if the current time is greater than when the shooting cycle is disabled to and make sure it is not (hence the !) is under the influence of DISARM
         return (Time.time >= ShootingCycleDisabled); //&& !currentInfluences[influences.DISARM]);
     }
 
-    public override void processHex(Hex h) {
+    public override void processHex(Hex h)
+    {
         h.aiCollide(gameObject);
         ApplyDamage(h.Damage);
         h.Destroy();
-        if (IsDead()) {
+        if (IsDead())
+        {
             Destroy(gameObject);
         }
     }
 
-    private ArrayList isInDanger() {
+    private ArrayList isInDanger()
+    {
         // Get all spells from mutual enemies
         ArrayList dangerousSpells = new ArrayList();
-        foreach (GameObject target in Targets) {
-            if (MutualTargets(target)) {
+        foreach (GameObject target in Targets)
+        {
+            if (MutualTargets(target))
+            {
                 HashSet<Hex> spells = target.GetComponent<ControlEntity>().ActiveHexes;
-                foreach (Hex h in spells) {
+                foreach (Hex h in spells)
+                {
                     if (h == null) continue;
                     GameObject spell = h.gameObject;
                     //for some reason the spells array consistently had hexes with no rigibodies in it 
-                    if (spell.gameObject.GetComponent<Rigidbody>() != null && Physics.Raycast(spell.transform.position, spell.gameObject.GetComponent<Rigidbody>().velocity.normalized, 50F, 1 << 8)) {
+                    if (spell.gameObject.GetComponent<Rigidbody>() != null && Physics.Raycast(spell.transform.position, spell.gameObject.GetComponent<Rigidbody>().velocity.normalized, 50F, 1 << 8))
+                    {
                         dangerousSpells.Add(spell);
                     }
                 }
@@ -241,11 +260,14 @@ public class StateAI : ControlEntity {
         return dangerousSpells;
     }
 
-    public void setSpeed(float newSpeed) {
-        if (newSpeed >= 0) {
+    public void setSpeed(float newSpeed)
+    {
+        if (newSpeed >= 0)
+        {
             speed = newSpeed;
         }
-        else {
+        else
+        {
             speed = defaultSpeed;
         }
     }
