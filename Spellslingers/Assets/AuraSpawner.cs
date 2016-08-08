f957feac0e7b6057add4b3a67e7e127c58593824
George Higashiyama <george@higashiyama.us>
2016-08-08 12:00:12 -0700

    Begin testing of random aura spawning

---
+++ Spellslingers/Assets/AuraSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AuraSpawner : MonoBehaviour {
+
+    public TargetableAura[] SpawnableAuras;
+    public GameObject[] AuraTargeters;
+
+   void Start() {
+        InvokeRepeating("SpawnAura", 5F, 20F);
+    }
+
+
+    void SpawnAura() {
+        Transform[] attachPoints = this.gameObject.GetComponentsInChildren<Transform>();
+        System.Random rnd = new System.Random();
+        int randomIndex = rnd.Next(0, attachPoints.Length);
+        TargetableAura auraTemplate = SpawnableAuras[rnd.Next(0, SpawnableAuras.Length)];
+        GameObject aura = Instantiate(auraTemplate, attachPoints[randomIndex].transform.position, Quaternion.identity) as GameObject;
+        aura.transform.SetParent(attachPoints[randomIndex], false);
+        foreach(GameObject ob in AuraTargeters) {
+            ob.GetComponent<ControlEntity>().AddTarget(aura);
+        }
+    }
+}
