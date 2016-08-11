e8f43870b3a596c227d6210bb4ef165d37458f2c
EspoCar <espomachine@gmail.com>
2016-08-11 12:13:57 -0700

    Basic Menu completed, creation of tutorial scene + testing.

--- Spellslingers/Assets/MenuObject.cs
+++ Spellslingers/Assets/MenuObject.cs
@@ -3,9 +3,9 @@ using System.Collections;
 
 public class MenuObject : MonoBehaviour {
     public string ScenetoLoad;
-	
         void OnTriggerEnter(Collider other)
     {
+
         Application.LoadLevel(ScenetoLoad);
     }
 }
