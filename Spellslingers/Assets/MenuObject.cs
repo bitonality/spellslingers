244d9ee96acb20f68de0253709d44c06bcc90a7e
EspoCar <espomachine@gmail.com>
2016-08-11 11:31:22 -0700

    Menu work, testing needed.

---
+++ Spellslingers/Assets/MenuObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuObject : MonoBehaviour {
+    public string ScenetoLoad;
+	
+        void OnTriggerEnter(Collider other)
+    {
+        Application.LoadLevel(ScenetoLoad);
+    }
+}
