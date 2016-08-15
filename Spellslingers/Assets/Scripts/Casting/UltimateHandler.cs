78aa2316e8c1937cba4e85d824cd4aa0d561ed1c
George Higashiyama <george@higashiyama.us>
2016-08-15 11:17:02 -0700

    Initial testing of ultimate casting

--- Spellslingers/Assets/Scripts/Casting/UltimateHandler.cs
+++ Spellslingers/Assets/Scripts/Casting/UltimateHandler.cs
@@ -3,63 +3,121 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 public class UltimateHandler : MonoBehaviour {
-
-    public Dictionary<GameObject, UltimateControllerData> ControllerMap;
-    public List<UltimateZoneChange> LoggedZones;
-    public List<UltimateInspectorEntry> UltimatePatterns;
-
+    [HideInInspector]
+    public UltimatePattern UserPattern = new UltimatePattern();
+    public UltimatePattern[] PatternTemplates;
+    public List<UltimateZoneChange> ControllerChange = new List<UltimateZoneChange>();
+    public Dictionary<GameObject, Queue<UltimateZone>> ControllerMap = new Dictionary<GameObject, Queue<UltimateZone>>();
 
     public void ZoneEntered(GameObject zone, GameObject controller) {
-       UltimateControllerData controllerData;
-        if(ControllerMap.TryGetValue(zone, out controllerData)) {
-            controllerData.TouchedZones.Enqueue(zone.GetComponent<UltimateZone>());
-        } else {
-            controllerData = new UltimateControllerData();
-            controllerData.TouchedZones.Enqueue(zone.GetComponent<UltimateZone>());
+
+        Queue<UltimateZone> controllerData;
+        if (ControllerMap.TryGetValue(zone, out controllerData)) {
+            controllerData.Enqueue(zone.GetComponent<UltimateZone>());
+        }
+        else {
+            controllerData = new Queue<UltimateZone>();
+            controllerData.Enqueue(zone.GetComponent<UltimateZone>());
             ControllerMap.Add(controller, controllerData);
         }
 
-        foreach (KeyValuePair<GameObject, UltimateControllerData> controllerEntry in ControllerMap) {
+        foreach (KeyValuePair<GameObject, Queue<UltimateZone>> controllerEntry in ControllerMap) {
             // If we have more than 1 zone enqueued.
-            if(controllerEntry.Value.TouchedZones.Count > 1) {
-
-                ZoneChange(controllerEntry.Value.TouchedZones.Dequeue(), controllerEntry.Value.TouchedZones.Dequeue(), controllerEntry.Key);
+            if (controllerEntry.Value.Count > 1) {
+                ZoneChange(controllerEntry.Value.Dequeue(), controllerEntry.Value.Dequeue(), controllerEntry.Key);
             }
         }
-
 
     }
 
     public void ZoneChange(UltimateZone from, UltimateZone to, GameObject controller) {
-            LoggedZones.Add(new UltimateZoneChange(from.ZoneID, to.ZoneID));
-            
-           
+        UltimateZoneChange change = new UltimateZoneChange(from.ZoneID, to.ZoneID, Time.time + 0.5F, controller);
+        ControllerChange.Add(change);
+
+        int listSize = ControllerChange.Count;
+
+        for(int i = listSize - 1; i > 0; i--) {
+            // If the point is expired, then remove it from the list and continue.
+            if (Time.time > ControllerChange[i].InvalidTime) {
+                ControllerChange.RemoveAt(i);
+                continue;
+            }
+
+            if((i-1) > -1 && ControllerChange[i].Movement == ControllerChange[i-1].Movement && ControllerChange[i].Controller != ControllerChange[i-1].Controller) {
+                ZoneMovement m = ControllerChange[i].Movement;
+                if(m == ZoneMovement.LEFT && UserPattern.First == ZoneMovement.UNDEFINED) {
+                    UserPattern.First = ZoneMovement.BOTH_LEFT;
+                }
+                else if (m == ZoneMovement.RIGHT && UserPattern.First == ZoneMovement.UNDEFINED) {
+                    UserPattern.First = ZoneMovement.BOTH_RIGHT;
+                }
+                else if (m == ZoneMovement.UP && UserPattern.Second == ZoneMovement.UNDEFINED) {
+                    UserPattern.Second = ZoneMovement.BOTH_UP;
+                }
+                else if (m == ZoneMovement.DOWN && UserPattern.Second == ZoneMovement.UNDEFINED) {
+                    UserPattern.Second = ZoneMovement.BOTH_DOWN;
+                }
+                ControllerChange.RemoveAt(i);
+                ControllerChange.RemoveAt(i - 1);
+            }
+
+            foreach(UltimatePattern pattern in PatternTemplates) {
+                if(pattern.First == UserPattern.First && pattern.Second == UserPattern.Second) {
+                    Debug.Log("Casting: " + pattern.Name);
+                }
+            }
+        }
+
+
+
+        
+
     }
 
     [System.Serializable]
-    public class UltimateInspectorEntry {
-        public UltimateZoneChange[] Changes;
+    public class UltimatePattern {
+        public string Name;
+        public ZoneMovement First;
+        public ZoneMovement Second;
         public GameObject Ultimate;
     }
 
+    public class UltimateZoneChange {
+        public int From;
+        public int To;
+        // A time in the future when the zone change should be discarded from the master queue.
+        public float InvalidTime;
+        public GameObject Controller;
+        public ZoneMovement Movement;
 
-    public class UltimateControllerData {
-        public Queue<UltimateZone> TouchedZones;
-        public UltimateControllerData() {
-            TouchedZones = new Queue<UltimateZone>();
+        public UltimateZoneChange(int from, int to, float invalidTime, GameObject controller) {
+            this.From = from;
+            this.To = to;
+            this.InvalidTime = invalidTime;
+            this.Controller = controller;
+
+            if(from - to == -1) {
+                this.Movement = ZoneMovement.LEFT;
+            } else if(from - to == 1) {
+                this.Movement = ZoneMovement.RIGHT;
+            } else if(from - to == 2) {
+                this.Movement = ZoneMovement.UP;
+            } else if(from - to == -2) {
+                this.Movement = ZoneMovement.DOWN;
+            }
         }
-
     }
 
-    public struct UltimateZoneChange {
-        public int from;
-        public int to;
-
-        public UltimateZoneChange(int from, int to) {
-            this.from = from;
-            this.to = to;
-        }
+    
+    public enum ZoneMovement {
+        UNDEFINED,
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN,
+        BOTH_LEFT,
+        BOTH_RIGHT,
+        BOTH_UP,
+        BOTH_DOWN,
     }
-
-
 }
