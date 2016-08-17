c5e84629518872962327bdb7ad0346082cd91f1f
EspoCar <espomachine@gmail.com>
2016-08-17 12:20:35 -0700

    credit updates, tutorial scripts created to assist in targetting

---
+++ Spellslingers/Assets/TutorialTargetting.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialTargetting : MonoBehaviour {
+
+    public GameObject Enemy1;
+    public GameObject Enemy2;
+    public GameObject Aura;
+     void Start()
+    {
+        gameObject.GetComponent<ControlEntity>().AddTarget(Enemy1);
+    }
+ public void tutorialSecondStage()
+    {
+        gameObject.GetComponent<ControlEntity>().CurrentTarget().GetComponent<ControlEntity>().ApplyDamage(10000); // Kills first entity
+        gameObject.GetComponent<ControlEntity>().AddTarget(Enemy2);
+        gameObject.GetComponent<ControlEntity>().AddTarget(Aura);
+    }   
+    
+}
