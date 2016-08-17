cd4efb2a3e675eca55122747fb97c5819a3f5405
EspoCar <espomachine@gmail.com>
2016-08-17 12:48:00 -0700

    AI version 2 uploaded, black with eyeball and is spooky, tutorial script modified and in need of testing

--- Spellslingers/Assets/HexToAura.cs
+++ Spellslingers/Assets/HexToAura.cs
@@ -10,6 +10,7 @@ public class HexToAura : MonoBehaviour {
            gameObject.GetComponent<ControlEntity>().AddTarget(Enemy1);
     }
         void OnTriggerEnter(Collider other){
+            Debug.Log("I'm triggered");
             gameObject.GetComponent<ControlEntity>().CurrentTarget().GetComponent<ControlEntity>().ApplyDamage(10000); // Kills first entity
             gameObject.GetComponent<ControlEntity>().AddTarget(Enemy2);
             gameObject.GetComponent<ControlEntity>().AddTarget(Aura);
--- Spellslingers/Assets/MenuObject.cs
+++ Spellslingers/Assets/MenuObject.cs
@@ -2,13 +2,9 @@
 using System.Collections;
 
 public class MenuObject : MonoBehaviour {
-    void Start (){
-       PlayerPrefs.SetInt("difficulty", 2); // Initialize this here so we don't have problems later down the line in case of difficulty returning null. Default difficulty : Medium
-    }
     public string ScenetoLoad;
         void OnTriggerEnter(Collider other)
     {
-
-        Application.LoadLevel(ScenetoLoad);
+   Application.LoadLevel(ScenetoLoad);
     }
 }
