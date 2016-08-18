817faae17245126f0657d41843fe2665ac399ac3
EspoCar <espomachine@gmail.com>
2016-08-18 13:19:54 -0700

    HExtoaurachanges, needs testing, involving using the other.gameobject on colliders

--- Spellslingers/Assets/HexToAura.cs
+++ Spellslingers/Assets/HexToAura.cs
@@ -7,12 +7,13 @@ public class HexToAura : MonoBehaviour {
     public GameObject Aura;
         void Start()
     {
-        GameObject.Find("Camera").GetComponent<ControlEntity>().AddTarget(Enemy1);
+       
+        gameObject.GetComponentInParent<ControlEntity>().AddTarget(Aura);
     }
         void OnTriggerEnter(Collider other){
             Debug.Log("I'm triggered");
-            gameObject.GetComponent<ControlEntity>().CurrentTarget().GetComponent<ControlEntity>().ApplyDamage(10000); // Kills current target of entity. 
-            gameObject.GetComponent<ControlEntity>().AddTarget(Enemy2);
-            gameObject.GetComponent<ControlEntity>().AddTarget(Aura);
+            other.gameObject.GetComponentInParent<ControlEntity>().CurrentTarget().GetComponent<ControlEntity>().ApplyDamage(10000); // Kills current target of entity. 
+            other.gameObject.gameObject.GetComponentInParent<ControlEntity>().AddTarget(Enemy2);
+            gameObject.GetComponentInParent<ControlEntity>().AddTarget(Aura);
         }
     }
